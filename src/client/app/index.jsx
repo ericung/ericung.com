e0ab2e9cfffc6583749e207ccbf85ae013eb2185
Eric Ung <ungericw@tutanota.com>
2017-03-06 18:31:39 -0600

    set status navbar

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -10,24 +10,41 @@ class NavMenu extends React.Component {
     render() {
         return (
             <Nav bsStyle="pills">
-                <NavItem onClick={() => alert('Home')}>Home</NavItem>
-                <NavItem onClick={() => alert('Posts')}>Posts</NavItem>
-                <NavItem onClick={() => alert('Projects')}>Projects</NavItem>
+                <NavItem onClick={() => this.props.onClick('Home')}>Home</NavItem>
+                <NavItem onClick={() => this.props.onClick('Posts')}>Posts</NavItem>
+                <NavItem onClick={() => this.props.onClick('Projects')}>Projects</NavItem>
             </Nav>
         );
     }
 }
 
+class Content extends React.Component {
+    render() {
+        return (
+            <p>{this.props.value}</p>
+        );
+    }
+}
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            content: "This is content"
+        }
+    }
+    handleClick(i) {
+        this.setState({content: i});
+    }
     render() {
         return (
         <div>
-            <NavMenu/>
-            <div className="content"></div>
+            <NavMenu onClick={(i)=>this.handleClick(i)} />
+            <Content value={this.state.content}/>
         </div>
         );
     }
 }
 
-
 ReactDOM.render(<App/>, document.getElementById('app'));
+
